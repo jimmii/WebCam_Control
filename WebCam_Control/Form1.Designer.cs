eee7cae445ceacf05f17503172efe04329301c8a
Damian <jimmii@o2.pl>
2019-02-26 21:38:37 +0100

    Capture Video

--- WebCam_Control/Form1.Designer.cs
+++ WebCam_Control/Form1.Designer.cs
@@ -28,83 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Start_Btn = new System.Windows.Forms.Button();
+            this.Stop_Btn = new System.Windows.Forms.Button();
+            this.SelectCamera_comboBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Display_pictureBox = new System.Windows.Forms.PictureBox();
+            this.TakePhoto_Btn = new System.Windows.Forms.Button();
+            this.CaptureVideo_Btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Display_pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // Start_Btn
             // 
-            this.button1.BackColor = System.Drawing.Color.Lime;
-            this.button1.Location = new System.Drawing.Point(39, 32);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = false;
+            this.Start_Btn.BackColor = System.Drawing.Color.Lime;
+            this.Start_Btn.Location = new System.Drawing.Point(39, 32);
+            this.Start_Btn.Name = "Start_Btn";
+            this.Start_Btn.Size = new System.Drawing.Size(131, 50);
+            this.Start_Btn.TabIndex = 0;
+            this.Start_Btn.Text = "Start";
+            this.Start_Btn.UseVisualStyleBackColor = false;
+            this.Start_Btn.Click += new System.EventHandler(this.Start_Btn_Click);
             // 
-            // button2
+            // Stop_Btn
             // 
-            this.button2.BackColor = System.Drawing.Color.Crimson;
-            this.button2.Location = new System.Drawing.Point(1014, 32);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 50);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Stop";
-            this.button2.UseVisualStyleBackColor = false;
+            this.Stop_Btn.BackColor = System.Drawing.Color.Crimson;
+            this.Stop_Btn.Location = new System.Drawing.Point(1014, 32);
+            this.Stop_Btn.Name = "Stop_Btn";
+            this.Stop_Btn.Size = new System.Drawing.Size(131, 50);
+            this.Stop_Btn.TabIndex = 1;
+            this.Stop_Btn.Text = "Stop";
+            this.Stop_Btn.UseVisualStyleBackColor = false;
+            this.Stop_Btn.Click += new System.EventHandler(this.Stop_Btn_Click);
             // 
-            // comboBox1
+            // SelectCamera_comboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(513, 46);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 2;
+            this.SelectCamera_comboBox.FormattingEnabled = true;
+            this.SelectCamera_comboBox.Location = new System.Drawing.Point(513, 46);
+            this.SelectCamera_comboBox.Name = "SelectCamera_comboBox";
+            this.SelectCamera_comboBox.Size = new System.Drawing.Size(168, 24);
+            this.SelectCamera_comboBox.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(523, 26);
+            this.label1.Location = new System.Drawing.Point(553, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Select Camera";
             // 
-            // pictureBox1
+            // Display_pictureBox
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(220, 112);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(730, 571);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.Display_pictureBox.Location = new System.Drawing.Point(220, 112);
+            this.Display_pictureBox.Name = "Display_pictureBox";
+            this.Display_pictureBox.Size = new System.Drawing.Size(730, 571);
+            this.Display_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Display_pictureBox.TabIndex = 4;
+            this.Display_pictureBox.TabStop = false;
             // 
-            // button3
+            // TakePhoto_Btn
             // 
-            this.button3.BackColor = System.Drawing.Color.DodgerBlue;
-            this.button3.Location = new System.Drawing.Point(235, 703);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(213, 38);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Take Photo";
-            this.button3.UseVisualStyleBackColor = false;
+            this.TakePhoto_Btn.BackColor = System.Drawing.Color.DodgerBlue;
+            this.TakePhoto_Btn.Location = new System.Drawing.Point(235, 703);
+            this.TakePhoto_Btn.Name = "TakePhoto_Btn";
+            this.TakePhoto_Btn.Size = new System.Drawing.Size(213, 38);
+            this.TakePhoto_Btn.TabIndex = 5;
+            this.TakePhoto_Btn.Text = "Take Photo";
+            this.TakePhoto_Btn.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // CaptureVideo_Btn
             // 
-            this.button4.BackColor = System.Drawing.Color.DodgerBlue;
-            this.button4.Location = new System.Drawing.Point(716, 703);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(213, 38);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "Capture Video";
-            this.button4.UseVisualStyleBackColor = false;
+            this.CaptureVideo_Btn.BackColor = System.Drawing.Color.DodgerBlue;
+            this.CaptureVideo_Btn.Location = new System.Drawing.Point(716, 703);
+            this.CaptureVideo_Btn.Name = "CaptureVideo_Btn";
+            this.CaptureVideo_Btn.Size = new System.Drawing.Size(213, 38);
+            this.CaptureVideo_Btn.TabIndex = 6;
+            this.CaptureVideo_Btn.Text = "Capture Video";
+            this.CaptureVideo_Btn.UseVisualStyleBackColor = false;
             // 
             // WebCam_Control
             // 
@@ -112,17 +114,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(1182, 753);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CaptureVideo_Btn);
+            this.Controls.Add(this.TakePhoto_Btn);
+            this.Controls.Add(this.Display_pictureBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SelectCamera_comboBox);
+            this.Controls.Add(this.Stop_Btn);
+            this.Controls.Add(this.Start_Btn);
             this.Name = "WebCam_Control";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WebCam Control";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WebCam_Control_FormClosing);
+            this.Load += new System.EventHandler(this.WebCam_Control_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Display_pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,13 +134,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Start_Btn;
+        private System.Windows.Forms.Button Stop_Btn;
+        private System.Windows.Forms.ComboBox SelectCamera_comboBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox Display_pictureBox;
+        private System.Windows.Forms.Button TakePhoto_Btn;
+        private System.Windows.Forms.Button CaptureVideo_Btn;
     }
 }
 
--- WebCam_Control/Form1.cs
+++ WebCam_Control/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace WebCam_Control
 {
@@ -15,6 +18,54 @@ namespace WebCam_Control
         public WebCam_Control()
         {
             InitializeComponent();
+        }
+
+        private FilterInfoCollection AvailableVideoDevices;
+        private VideoCaptureDevice VideoSource;
+
+        private void WebCam_Control_Load(object sender, EventArgs e)
+        {
+            AvailableVideoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo Device in AvailableVideoDevices)
+            {
+                SelectCamera_comboBox.Items.Add(Device.Name);
+            }
+            SelectCamera_comboBox.SelectedItem = 0;
+
+            VideoSource = new VideoCaptureDevice();
+        }
+
+        private void Start_Btn_Click(object sender, EventArgs e)
+        {
+            if (SelectCamera_comboBox.SelectedIndex >= 0)
+            {             
+                    VideoSource = new VideoCaptureDevice(AvailableVideoDevices[SelectCamera_comboBox.SelectedIndex].MonikerString);
+                    VideoSource.NewFrame += new NewFrameEventHandler(VideoSource_NewFrame);
+                    VideoSource.Start();
+                
+            }           
+            else
+                MessageBox.Show("You must select camera!", "Info", MessageBoxButtons.OK);
+        }
+
+            private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+            {
+            Display_pictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+            }
+
+        private void WebCam_Control_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if ((VideoSource.IsRunning == true) && (VideoSource!=null))
+                VideoSource.Stop();
+        }
+
+        private void Stop_Btn_Click(object sender, EventArgs e)
+        {
+            if (VideoSource.IsRunning == true)
+                {
+                    VideoSource.Stop();
+                    VideoSource = null;
+                }   
         }
     }
 }
